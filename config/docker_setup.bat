83e7a6b0bb4ff297a974d34efe78992e7a511358
KodinGoose <157022914+KodinGoose@users.noreply.github.com>
2026-02-04 04:34:04 +0100

    Delete config/docker_setup.bat

--- config/docker_setup.bat
+++
@@ -1,9 +0,0 @@
-echo "Make sure that docker desktop is installed\n"
-
-cd ../backend
-docker build . -t ordayna-backend
-echo "database:3306" > database_address
-cd ../config
-
-echo "Run by double clicking on run_on_windows.bat in the project root folder"
-
