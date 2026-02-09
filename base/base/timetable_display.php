c2234d80efe698ec8c81d91955b19a91dae5dd31
kluvc06 <157023155+kluvc06@users.noreply.github.com>
2026-02-09 10:15:17 +0100

    Delete base directory

--- base/base/timetable_display.php
+++
@@ -1,6 +0,0 @@
-<!--I DONT KNOW-->
-<?php include 'header.php'?>
-<?php echo "bye" ?>
-
-<br><button onclick="location.href='index.php'" id="home_button">home</button>
-<button onclick="location.href='profile.php'" id="user_button">Felhasználó</button>
--- base/timetable_display.php
+++
@@ -1,6 +0,0 @@
-<!--I DONT KNOW-->
-<?php include 'stylesheet_call.php'?>
-<?php echo "bye" ?>
-
-<br><button onclick="location.href='index.php'" id="home_button">home</button>
-<button onclick="location.href='profile.php'" id="user_button">Felhasználó</button>
